b34fca2855faf8844ca28fdc601ee04856c59d74
unknown <yuxi@YUXMED0122L.internal.apexcorp.local>
2018-02-24 13:59:20 -0500

    day3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  strString : string;
+  constructor(private httpClient: HttpClient) { }
+  ngOnInit(){
+    this.response();
+  }
+
+  response() : void{
+    setInterval( () =>{
+      this.httpClient.get <any> ('https://api.icndb.com/jokes/random').subscribe(data => {
+        console.log(data.value.joke)
+        this.strString = data.value.joke;
+      });
+    }, 5000);
+  }
 }
